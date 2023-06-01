8b7d1952e2a87b1b969e3b46dda2ed07b412b371
Michael Rodler <mrodler@amazon.de>
2023-06-01 08:44:22 +0000

    initial

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+fn file_thingy() {
+    let file_path = "./whatever";
+    println!("In file {}", file_path);
+
+    let contents = if std::path::Path::new(file_path).exists() {
+        // can panic if unwr
+        fs::read_to_string(file_path).unwrap()
+    } else {
+        "".to_string()
+    };
+
+    println!("With text:\n{contents}");
+}
+
+fn env_var_thingy() {
+    if let Ok(var) = std::env::var("TEST") {
+        if let Ok(i) = var.parse::<i32>() {
+            match i {
+                0..=10 => println!("small"),
+                11..=100 => println!("mid-size"),
+                i32::MAX => panic!("no no no"),
+                _ => println!("large"),
+            }
+        }
+    }
+
+    let var = std::env::var("TEST2");
+    if var.is_ok() {
+        let var = var.unwrap();
+        println!("{var}");
+    }
+}
+
+fn main() {
+    env_var_thingy();
+    file_thingy();
+}
